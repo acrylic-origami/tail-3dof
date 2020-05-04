4b3fbd604edf4497e5e0c09dd55c1c470486d737
Derek Lam <dc.lam@mail.utoronto.ca>
2020-05-04 12:55:17 -0700

    Int math debugging: sqrts cut the mantissa in half, not by constant amount

--- stm/f072_kin_test/Src/kin.c
+++ stm/f072_kin_test/Src/kin.c
@@ -26,19 +26,19 @@ void fwd_kin(int32_t *A, int32_t *q, int32_t *r) {
     r[2] = (s[0] * v >> _W) + ((A[3] * c[0] >> _W) * s[2] >> _W);
 }
 uint8_t inv_kin(int32_t *A, int32_t *T, int32_t *t) {
-    int32_t rT = 0;
+    volatile int32_t rT = 0;
     for(uint8_t i = 0; i < 3; i++) {
         rT += T[i] * T[i] >> _W;
     }
     rT = isqrt(rT) << _HW;
-    int32_t r_yz = isqrt((T[1] * T[1] >> _W) + (T[2] * T[2] >> _W)) << _HW;
-    int32_t c1 = (r_yz << _W) / rT;
+    volatile int32_t r_yz = isqrt(T[1] * T[1] + T[2] * T[2]); // WATCH multiply with sqrt
+    volatile int32_t c1 = (r_yz << _W) / rT;
 
-    int32_t r0 = A[1] + (c1 * A[2] >> _W);
-	int32_t r1 = c1 * A[3] >> _W;
-	int32_t r2 = A[3];
+    volatile int32_t r0 = A[1] + (c1 * A[2] >> _W);
+	volatile int32_t r1 = c1 * A[3] >> _W;
+	volatile int32_t r2 = A[3];
 
-	int32_t y;
+	volatile int32_t y;
 
 	if((1 << _W) - c1 < EPS) {
 		// degenerate case: in the y-z plane already, so solve directly
@@ -48,12 +48,12 @@ uint8_t inv_kin(int32_t *A, int32_t *T, int32_t *t) {
 		y = (r0 * r0 - r1 * r1) / 2 / r_yz; // EXEMPT: * followed by /, along with distributivity
 	}
 	else {
-	    int32_t pc[3] = {
+	    volatile int32_t pc[3] = {
 	        (1 << _TW) / (r1 * r1 >> _W) - (1 << _TW) / (r2 * r2 >> _W),
 			-2 * (r_yz << _W) / (r1 * r1 >> _W),
 			r0 * r0 / (r2 * r2 >> _W) + (r_yz * r_yz / (r1 * r1 >> _W)) - (1 << _W)
 	    };
-	    int32_t det = pc[1] * pc[1] - 4 * pc[0] * pc[2] >> _W;
+	    volatile int32_t det = pc[1] * pc[1] - 4 * pc[0] * pc[2] >> _W;
 	    if(det < 0)
 	        return 1;
 
@@ -64,13 +64,13 @@ uint8_t inv_kin(int32_t *A, int32_t *T, int32_t *t) {
 	        return 1;
 	}
 
-    int32_t z = isqrt(r0 * r0 - y * y) >> _HW; // WATCH multiply with sqrt
+    volatile int32_t z = isqrt(r0 * r0 - y * y); // WATCH multiply with sqrt
 
-    int32_t m0 = (z << _W) / r0;
-    int32_t m1 = -(1 << _TW) / m0;
-    int32_t y_ = (m1 * r_yz) / (m1 - m0); // WATCH multiply with divide
-    int32_t z_ = m0 * y_ >> _W;
-    int32_t r_yz_ = isqrt(y_ * y_ + z_ * z_) >> _HW; // WATCH multiply with sqrt
+    volatile int32_t m0 = (z << _W) / r0;
+    volatile int32_t m1 = -(1 << _TW) / m0;
+    volatile int32_t y_ = (m1 * r_yz) / (m1 - m0); // WATCH multiply with divide
+    volatile int32_t z_ = m0 * y_ >> _W;
+    volatile int32_t r_yz_ = isqrt(y_ * y_ + z_ * z_); // WATCH multiply with sqrt
 
     if(1 - c1 < EPS)
     	t[1] = 0; // optimizing (perhaps prematurely
